72cc537838e7284c3ce61d985a4a0ffe2c8c541a
abdoudiarra <58839578+abdoudiarra@users.noreply.github.com>
2023-08-25 08:56:50 -0400

    learning about state management (useState)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+  const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+  ];
+
+
+export default function App(){
+
+  const [step, stepState] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  //const [test, handleName] = useState({name : "Abdou"}); 
+
+  function handlePrevious(){
+    if(step > 1) stepState(step - 1);
+  }
+  function handleNext(){
+    if(step < 3) stepState(step + 1);
+
+    //BAD PRACTICE
+    //test.name = "testname";
+
+    //handleName({name: "bob"})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+          &times;
+      </button>
+
+
+      {isOpen && (
+      <div className="steps">
+        <div className="numbers">
+          <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+          <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+          <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        </div>
+
+        <p className="message">Step {step}: {messages[step-1]} </p>
+
+        <div className="buttons">
+          <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handlePrevious}>
+            Previous
+          </button>
+          <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handleNext}>
+            Next
+          </button>
+        </div>
+      </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
